f9f4e37a6a8e4e635116baf7e3467efc888e4a59
matthart10 <170780306+matthart10@users.noreply.github.com>
2025-01-10 19:33:00 -0800

    Add in comments for readability, button and form
    Add in comments in javascript to be able to easily go through code and
    figure out what is happening. Add in a button with the same styling as
    the book divs that will eventually on click bring up a form. Start to
    make a form and eventually style and then hide it

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-/* All book objects will be stored in myLibrary, an array */
-
+// All book objects will be stored in myLibrary, an array
 const myLibrary = [];
 
-/* This is our object constructor, all book objects will have this as its prototype */
-
+// This is our object constructor, all book objects will have this as its prototype
 function Book(image, title, author, pages, read) {
     this.image = image;
     this.title = title;
@@ -12,21 +10,32 @@ function Book(image, title, author, pages, read) {
     this.read = read;
 }
 
+// Function that takes in a variable (which is usually a book in the form of an object) and adds it to the array
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+// Creating Inner Game of Tennis object with Book object constructor
 const innerGame = new Book('./innerGame.jpg', 'The Inner Game of Tennis', 'W. Timothey Gallwey', 192, 'Yes');
 
+// Add Inner Game of Tennis to library by calling this function
 addBookToLibrary(innerGame);
 
-
+// Select the div that contains all the cards
 let shelfSelection = document.querySelector("#shelf");
-const newCard = document.createElement("div");
 
+// Get array length
 var myLibraryLength = myLibrary.length;
 
+// For loop that loops through array length and creates a div/card for each object
 for (var i = 0; i < myLibraryLength; i++) {
+    const newCard = document.createElement("div");
     newCard.innerHTML = "<img src=" + (myLibrary[i].image) + " alt='book cover'>" + "<strong>" + "Name: " + "</strong>" + (myLibrary[i].title) + " by " + (myLibrary[i].author) + "<br />" + "<strong>" + " Pages: " + "</strong>" + (myLibrary[i].pages) + "<br />" + "<strong>" + " Read? " + "</strong>" + (myLibrary[i].read)
     shelfSelection.appendChild(newCard);
 };
+
+// Add a separate card that is a button so you can click it to add a book
+const addBookButton = document.createElement("button");
+addBookButton.classList.add("plus");
+addBookButton.innerHTML = "+";
+shelfSelection.appendChild(addBookButton);
